4635aa2c0b6735fcf6cfc53926736576738bb28c
Pimenov <pimenovvasiliy@gosniias.ru>
2021-10-10 15:30:37 +0300

    - Добавил решение задачи сортировки введенного массива

---
+++ week1/src/ru/croc/task3/Main.java
@@ -0,0 +1,10 @@
+package ru.croc.task3;
+
+import java.util.Arrays;
+
+class Main {
+	public static void main(String[] args) {
+		Arrays.sort(args);
+		System.out.print(Arrays.toString(args));
+	}
+}
