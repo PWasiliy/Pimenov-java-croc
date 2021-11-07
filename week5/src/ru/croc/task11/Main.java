ba26894466b28a282714359e86a81f2efa541388
Pimenov <pimenovvasiliy@gosniias.ru>
2021-11-07 14:21:33 +0300

    - Добавил решение 11-ой задачи, где нужно было разработать потокобезопасный класс для одного лота на аукционе

---
+++ week5/src/ru/croc/task11/Main.java
@@ -0,0 +1,33 @@
+package ru.croc.task11;
+
+import java.util.Date;
+
+public class Main {
+    static class Lot {
+        private volatile int price;
+        private volatile String buyer;
+        private Date finish;
+
+        public Lot(Date finish) {
+            this.finish = finish;
+        }
+
+        public synchronized boolean update(int price, String buyer) {
+            if ((price > this.price) && (new Date().before(this.finish))) {
+                this.price = price;
+                this.buyer = buyer;
+                return true;
+            } else return false;
+        }
+
+        public synchronized String getBuyer() {
+            return this.buyer;
+        }
+    }
+
+    public static void main(String[] args) {
+	    Lot lot = new Lot(new Date(System.currentTimeMillis() + 1_000_000));
+        lot.update(100, "Croc");
+        System.out.println(lot.getBuyer());
+    }
+}
