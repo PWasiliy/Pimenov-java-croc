2319f5e459bc68bce228fb7586211abcbf427f34
Pimenov <pimenovvasiliy@gosniias.ru>
2021-11-14 12:07:19 +0300

    Исправил решение 8-ой задачи, чтобы учитывались только слово в соотвествии с определением "\w"

--- week4/src/ru/croc/task8/Main.java
+++ week4/src/ru/croc/task8/Main.java
@@ -3,6 +3,8 @@ package ru.croc.task8;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,10 +16,12 @@ public class Main {
             System.out.printf("Не удалось открыть указанный файл, ошибка: \"%s\"", e.getMessage());
             return;
         }
-        
+
+        Pattern pattern = Pattern.compile("[a-zа-яA-ZА-Я_0-9]+");
         int count = 0;
         while (scanner.hasNextLine()) {
-            count += scanner.nextLine().split("\s{1,}").length;
+            Matcher matcher = pattern.matcher(scanner.nextLine());
+            while (matcher.find()) ++count;
         }
         System.out.printf("Количество слов в файле: %d", count);
     }
